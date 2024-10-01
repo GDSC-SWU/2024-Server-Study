6a2426169bace2da8bf194b3a7f3ba3773a63cec
hyojin425 <gywls5184@gmail.com>
2024-10-01 18:47:01 +0900

    2주차 실습

---
+++ src/main/java/com/example/gdg/TestService.java
@@ -0,0 +1,18 @@
+package com.example.gdg;
+
+import com.sun.source.tree.MemberReferenceTree;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TestService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
+    }
+}
