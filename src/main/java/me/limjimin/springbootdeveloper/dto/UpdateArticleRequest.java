92a02e149762d21540cabc194fbd2ecab513d632
JiminLim <jimin0190053@gmail.com>
2024-10-08 16:12:39 +0900

    ~CH06

---
+++ src/main/java/me/limjimin/springbootdeveloper/dto/UpdateArticleRequest.java
@@ -0,0 +1,13 @@
+package me.limjimin.springbootdeveloper.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateArticleRequest {
+    private String title;
+    private String content;
+}
