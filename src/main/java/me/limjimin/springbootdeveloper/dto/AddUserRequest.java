575ecbb0a2d77acea8687ff8076d8b7b0bf4c8b0
JiminLim <jimin0190053@gmail.com>
2024-11-04 13:53:09 +0900

    ~CH08

---
+++ src/main/java/me/limjimin/springbootdeveloper/dto/AddUserRequest.java
@@ -0,0 +1,11 @@
+package me.limjimin.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddUserRequest {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/me/limjimin/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,18 @@
+package me.limjimin.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.limjimin.springbootdeveloper.domain.User;
+import me.limjimin.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
