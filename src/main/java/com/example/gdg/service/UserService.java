a93dfaf83b3f1d83cd83e4cda6ae711b9ba74775
hyojin425 <gywls5184@gmail.com>
2024-11-05 18:42:33 +0900

    5주차 실습

---
+++ src/main/java/com/example/gdg/service/UserService.java
@@ -0,0 +1,23 @@
+package com.example.gdg.service;
+
+import com.example.gdg.domain.User;
+import com.example.gdg.dto.AddUserRequest;
+import com.example.gdg.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto){
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
